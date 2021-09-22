0279d1ff551f5b2fa0188ff2327e8e0044f2d52c
maxarvid <max.arvid.anderson@gmail.com>
2021-09-22 17:29:58 +0200

    fixes loading in of webmock, adds Net::HTTP calls and corresponding stub_request

--- spec/requests/api_can_analyze_text_spec.rb
+++ spec/requests/api_can_analyze_text_spec.rb
@@ -1,17 +1,27 @@
 require 'rails_helper'
 
-RSpec.describe 'POST api/analyses', type: :request do
+RSpec.describe 'POST https://safe-for-work-api.herokuapp.com/api/analyses', type: :request do
   before do
-    stub_request(:any, 'https://safe-for-work-api.herokuapp.com').to_return(body: './config/happy_text_response.json')
-    binding.pry
+    stub_request(:post, 'https://safe-for-work-api.herokuapp.com/api/analyses')
+      .with(body: { 'category' => 'text' },
+            headers: {
+              'Accept' => '*/*',
+              'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+              'Host' => 'safe-for-work-api.herokuapp.com',
+              'User-Agent' => 'Ruby'
+            })
+      .to_return(status: 200, body: '', headers: {})
   end
-
 
   subject { response }
 
   describe 'when safe for work text is called on the API' do
     before do
-      post '/api/analyses'
+      url = URI('https://safe-for-work-api.herokuapp.com/api/analyses')
+      params = URI.encode_www_form({ "category": 'text' })
+      #url.query = URI.encode_www_form(params)
+      #binding.pry
+      Net::HTTP.post(url, params)
     end
 
     it 'is expected to return a clean response' do
