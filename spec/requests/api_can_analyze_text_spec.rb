e2ce4e92fd113c74318cd3d0ac057c3e17f8be89
Dorian Buck <dorianbuck@gmail.com>
2021-09-22 18:26:58 +0200

    adds successfully accessed the fixture data

--- spec/requests/api_can_analyze_text_spec.rb
+++ spec/requests/api_can_analyze_text_spec.rb
@@ -10,22 +10,23 @@ RSpec.describe 'POST https://safe-for-work-api.herokuapp.com/api/analyses', type
               'Host' => 'safe-for-work-api.herokuapp.com',
               'User-Agent' => 'Ruby'
             })
-      .to_return(status: 200, body: '', headers: {})
+      .to_return(status: 200, body: File.new('./spec/fixtures/happy_test_response.json'), headers: {})
   end
 
   subject { response }
-
+let(:response) {}
   describe 'when safe for work text is called on the API' do
     before do
       url = URI('https://safe-for-work-api.herokuapp.com/api/analyses')
       params = URI.encode_www_form({ "category": 'text' })
-      #url.query = URI.encode_www_form(params)
-      #binding.pry
-      Net::HTTP.post(url, params)
+      # url.query = URI.encode_www_form(params)
+      response = Net::HTTP.post(url, params)
+      # binding.pry
     end
 
     it 'is expected to return a clean response' do
-      expect(eval(response_json['results']['classification'])[0]['tag_name']).to eq 'clean'
+      binding.pry
+      expect(eval(response)['results']['classification'][0]['tag_name']).to eq 'clean'
     end
   end
 end
